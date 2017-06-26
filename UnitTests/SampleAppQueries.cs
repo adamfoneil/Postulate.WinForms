cb571077123dfad1ae8995bbe2229a4e83af793f
Adam O'Neil <adamosoftware@gmail.com>
2017-06-26 09:59:32 -0400

    added unit test project, updated Postulate.Sql package

---
+++ UnitTests/SampleAppQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestWinForms.Queries;
+using Postulate.WinForms;
+using Postulate.Sql.Abstract;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SampleAppQueries
+    {
+        [TestMethod]
+        public void TestOrgSelect()
+        {
+            var qry = new OrgSelect();
+            qry.Test();
+        }
+    }
+}
