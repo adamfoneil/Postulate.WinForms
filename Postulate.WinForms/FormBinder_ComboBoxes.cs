39cb0affd326507f77bdd5648ca03e4eccac60bf
Adam O'Neil <adamosoftware@gmail.com>
2017-06-26 11:03:42 -0400

    defaults feature but not tested #4

--- Postulate.WinForms/FormBinder_ComboBoxes.cs
+++ Postulate.WinForms/FormBinder_ComboBoxes.cs
@@ -12,7 +12,7 @@ namespace Postulate.WinForms
 {
     public partial class FormBinder<TRecord, TKey> where TRecord : Record<TKey>, new()
     {
-        public void AddControl<TValue>(ComboBox control, Expression<Func<TRecord, object>> property)
+        public void AddControl<TValue>(ComboBox control, Expression<Func<TRecord, object>> property, TValue defaultValue = default(TValue))
         {
             PropertyInfo pi = GetProperty(property);
             Action<TRecord> writeAction = (record) =>
@@ -26,14 +26,26 @@ namespace Postulate.WinForms
                 control.SetValue((TValue)func.Invoke(record));
             };
 
-            AddControl(control, writeAction, readAction);
+            if (typeof(TValue).Equals(typeof(int)))
+            {
+                AddComboBox(control, writeAction, readAction, () => { control.SelectedIndex = -1; });
+            }
+            else
+            {                
+                AddComboBox(control, writeAction, readAction, () => { control.SetValue(defaultValue); });
+            }            
         }
 
         public void AddControl(ComboBox control, Action<TRecord> writeAction, Action<TRecord> readAction, int defaultValue = -1)
         {
+            AddComboBox(control, writeAction, readAction, () => { control.SelectedIndex = defaultValue; });
+        }
+
+        private void AddComboBox(ComboBox control, Action<TRecord> writeAction, Action<TRecord> readAction, Action clearAction)
+        {
             control.SelectedIndexChanged += delegate (object sender, EventArgs e) { ValueChanged(writeAction); };
             _readActions.Add(readAction);
-            _clearActions.Add(() => { control.SelectedIndex = defaultValue; });
+            _clearActions.Add(clearAction);
         }
     }
 }
